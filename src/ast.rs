656c5564a7cd9262ac52dc4dddbc2d51d0f3f2e0
Your Name <you@example.com>
2021-04-30 10:58:45 +0300

    Helper functions for AST

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashSet, iter::{self, Iterator}};
+
 pub type Pos = usize;                                                               
                                                                                 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]                                                                
@@ -34,4 +36,40 @@ pub enum Ast {
     LApp(Box<Ast>, Box<Ast>),
     RApp(Box<Ast>, Box<Ast>),
 }                                                                               
+
+impl Ast {
+    pub fn direct_children(&self) -> Vec<&Self> {
+        match self {
+            Ast::Paren(e, _) => vec![e.as_ref()],
+            Ast::Var(_, _) | Ast::IntLiteral(_, _) | Ast::BoolLiteral(_, _) => vec![],
+            Ast::And(l, r) | Ast::Eq(l, r) | Ast::Add(l, r) |
+            Ast::LApp(l, r) | Ast::RApp(l, r) => {
+                vec![l.as_ref(), r.as_ref()]
+            },
+            Ast::Let { right, body, .. } => vec![right.as_ref(), body.as_ref()],
+            Ast::Function { ret, .. } => vec![ret.as_ref()],
+        }
+    }
+
+    pub fn all_children(&self) -> impl Iterator<Item=&Self> {
+        let direct_children = self.direct_children();
+        let children = direct_children.into_iter().map(Self::all_children).flatten();
+        iter::once(self).chain(children)
+    }
+
+    pub fn free_variables(&self) -> HashSet<&String> {
+        match self {
+            Ast::Let { name, right, body, .. } => {
+                body.free_variables().into_iter().filter(|x| x != &name)
+                .chain(right.free_variables()).collect()
+            },
+            Ast::Function { input: (input_var, _), ret, .. } => {
+                ret.free_variables().into_iter().filter(|x| x != &input_var).collect()
+            },
+            Ast::Var(name, _) => [name].iter().cloned().collect(),
+            x => x.direct_children().into_iter().map(Self::free_variables).flatten().collect(),
+        }
+    }
+}
+
 
